3d28bb16c43d799153b694441c94cdd1f36d62b6
Birnir Þór <birnir@programm.is>
2025-12-22 20:52:52 +0000

    Added dummy key

--- main.cpp
+++ main.cpp
@@ -31,7 +31,12 @@ int main() {
     AESEncryptionSystem aes;
     std::string input;
 
-    uint8_t dummy_key[16] = {0 };
+    uint8_t dummy_key[16] = {
+		0x01, 0x02, 0x03, 0x04,
+		0x05, 0x06, 0x07, 0x08,
+		0x09, 0x0a, 0x0b, 0x0c,
+		0x0d, 0x0e, 0x0f, 0x10
+    };
     uint8_t out[16] = {0 };
 
     std::cout << "AES Controller Terminal\n";
